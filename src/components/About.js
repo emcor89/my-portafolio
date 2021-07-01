ab78f19363f5b0ddd0bc7e06039a225b93a69daf
Emilio Coronel <emcor89@gmail.com>
2021-07-01 17:17:36 -0500

    cambio about.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,25 +7,23 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Somos PinCode
+            <br className="hidden lg:inline-block" />Mejoramos tu imagen Digital
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            Somos una agencia dedicada a mejorar o contruir la imagen de tu marca en el mundo digital. 
+            Estamos aqui para darle valor y prestigio a tu marca que sea conocida por todos y hacerla crecer hasta donde te lo propongas.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              Trabaja con Nosotros
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+              Revisa nuestro trabajo
             </a>
           </div>
         </div>
